2247531a1fcb4974cd543c711ccffd931a4ad26c
Admin <Admin@DELL>
2020-07-20 20:03:51 +0530

    first commit

---
+++ src/main/java/com/in28minutes/In28minutesmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class In28minutesmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(In28minutesmicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in28minutes/In28minutesmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class In28minutesmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
