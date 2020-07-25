d8f1046e15f2480c86fbadd23616be3fd8b9f805
Admin <Admin@DELL>
2020-07-25 18:25:49 +0530

    last commit

--- src/main/java/com/in28minutes/In28minutesmicroserviceApplication.java
+++ src/main/java/com/in28minutes/In28minutesmicroserviceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class In28minutesmicroserviceApplication {
 
 	public static void main(String[] args) {
-		//abc
+		
 		SpringApplication.run(In28minutesmicroserviceApplication.class, args);
 	}
 
